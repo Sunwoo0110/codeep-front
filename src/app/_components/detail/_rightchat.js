11e6d1bcc5d580be1cf960c9e3db6d9fe9f3d336
sunwookim <sunkim09@g.skku.edu>
2024-07-08 05:28:53 +0900

    FEA chat api

--- src/app/_components/detail/_rightchat.js
+++ src/app/_components/detail/_rightchat.js
@@ -2,11 +2,12 @@
 
 import styles from "../../../styles/detail/_rightchat.module.css"
 
-export default function Rightchat({name, content}) {
+export default function Rightchat({name, content, point}) {
     return (
         <div className={styles.chatroom_right_container}>
             <div className={styles.chatroom_right_name}>{name}</div>
             <div className={styles.chatroom_right_content}>{content}</div>
+            <div className={styles.chatroom_right_point}>{`Point: ${point}`}</div>
         </div>
     )
 }
--- src/app/detail/[story_id]/[episode_order]/reasoning/page.js
+++ src/app/detail/[story_id]/[episode_order]/reasoning/page.js
@@ -11,23 +11,26 @@ import Rightchat from "../../../../_components/detail/_rightchat";
 export default function Reasoning() {
 
     const [maintitle, setMainTitle] = useState('대화를 통해 범인을 검거하세요!');
-    const [subtitle, setSubTitle] = useState('GPT 기반 추리형 영어 독해 학습 서비스');
+    const [subtitle, setSubTitle] = useState('위의 아이콘을 클릭하여 수집된 단서를 참고하세요.');
 
     const [nextButton, setNextButton] = useState('결말 확인하기');
-    const [isLastScript, setIsLastScript] = useState(false);
+    const [isLastChat, setIsLastChat] = useState(false);
     const [isDisabled, setIsDisabled] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const [content, setContent] = useState('');
-    const [chats, setChats] = useState([
-        { name: "Conan", content: "Who killed Narasaku?"}
-    ]);
-    const [order, setOrder] = useState(2);
-    const [scriptNum, setScriptNum] = useState(100);
+    const [question, setQuestion] = useState('');
+    const [chats, setChats] = useState([]);
+    const [point, setPoint] = useState(0);
+    const [totalPoint, setTotalPoint] = useState(0);
 
     const pathname = usePathname();
     const storyId = pathname.split("/")[2];
     const episodeOrder = pathname.split("/")[3];
     const chatContainerRef = useRef(null);
+
+    const level = window.localStorage.getItem("userLevel");
+    const userName = window.localStorage.getItem("userName");
 
     const handleKeyPress = (e) => {
         if (e.key === 'Enter' && !e.shiftKey) {
@@ -37,30 +40,121 @@ export default function Reasoning() {
     };
 
     const sendChat = async () => {
-
-        if (order <= scriptNum) {
-            setChats(currentChats => [...currentChats, { name: "user", content: content }]);
-            const res = await axios.get(`http://localhost:8000/scripts/get_script/${order}`);
-            setChats(currentChats => [...currentChats, { name: "Conan", content: res.data.script[0].content }]);
-            setContent('');
-            setScriptNum(res.data.script[0].num_of_scripts);
-
-            if (order === res.data.script[0].num_of_scripts) {
-                setIsLastScript(true);
-            }
-
-            setOrder(order + 1);
-        }
+        setIsLoading(true);
+        await getPoint(content);
+        console.log(chats);
+        setContent('');
+        setIsLoading(false); 
     }
 
     const nextClick = () => {
         window.location.href = `/detail/${storyId}/${parseInt(episodeOrder)+1}`
     }
 
-    useEffect(() => {
-        if (parseInt(episodeOrder) === order) {
-            window.localStorage.removeItem("collectedClueList");
+    const getPoint = async (input) => { 
+        
+        const res = await axios.post(`http://localhost:8000/points/detect_point`, {
+            story_id: storyId,
+            name: userName,
+            level: level,
+            question: chats[chats.length-1].content,
+            input: input
+        });
+
+        if (res.data.result === "success") {
+            setPoint(res.data.point.point);
+            setTotalPoint(res.data.point.total_point);
+
+            console.log(res.data.point.point);
+            console.log(res.data.point.total_point);
+
+            setChats(currentChats => [...currentChats, { name: userName, content: input, point: res.data.point.point}]);
+
+            await createChat(input, res.data.point.point);
+        } else if (res.data.result === "failure") {
+            // 채팅 끝내기
+            setIsLastChat(true);
+            setIsDisabled(true);
+            
+            setChats(currentChats => [...currentChats, { name: userName, content: input, point: 0}]);
+            setChats(currentChats => [...currentChats, { name: "Conan", content: res.data.content, point: 0}]);
+            
+        } else {
+            alert("오류가 발생하였습니다.\n다시 시도해주세요.");
         }
+        return 0;
+    }
+
+    const createChat = async (input, point) => {
+
+        if (input === "") {
+            alert("메세지를 입력해주세요.");
+        }
+        else {
+            const res = await axios.post(`http://localhost:8000/chattings/create_chat`, {
+                story_id: storyId,
+                name: userName,
+                level: level,
+                input: input,
+                point: point
+            });
+    
+            if (res.data.result === "success") {
+                setChats(currentChats => [...currentChats, { name: "Conan", content: res.data.chatting.content, point: point}]);
+                if (res.data.chatting.is_end === 1){
+                    setIsLastChat(true);
+                }
+            }
+        }
+    }
+
+    useEffect(() => {
+        const getChats = async () => {
+            const res = await axios.post(`http://localhost:8000/chattings/get_all_chat`, {
+                story_id: storyId,
+                name: userName,
+                level: level
+            });
+            if (res.data.result === "success") {
+                const transformedChats = res.data.chattings.map(chat => {
+                    return {
+                        name: chat.is_user === 0 ? "Conan" : userName,
+                        content: chat.chat,
+                        point: chat.point
+                    };
+                });
+                setChats(transformedChats);
+                console.log(transformedChats);
+
+                for (let i = 0; i < transformedChats.length; i++) {
+                    if (transformedChats[i].name === userName) {
+                        console.log(transformedChats[i].point);
+                        setTotalPoint(currentPoint => currentPoint + transformedChats[i].point);
+                    }
+                }
+            } else {
+                if (chats.length === 0) {
+                    setIsLoading(true);
+                    const res = await axios.post(`http://localhost:8000/chattings/create_chat`, {
+                        story_id: storyId,
+                        name: userName,
+                        level: level,
+                        point: 0
+                    });
+            
+                    if (res.data.result === "success") {
+                        setQuestion(res.data.chatting.content);
+                        setChats(currentChats => [...currentChats, { name: "Conan", content: res.data.chatting.content, point: 0}]);
+                        if (res.data.chatting.is_end === 1){
+                            setIsLastChat(true);
+                        }
+                    }
+                    setIsLoading(false);
+                }
+
+            }
+        }
+        getChats();
     }, []);
 
     useEffect(() => {
@@ -77,8 +171,14 @@ export default function Reasoning() {
                 <div className={styles.main_title}>
                     {maintitle}
                 </div>
+                <div className={styles.sub_title}>
+                    {subtitle}
+                </div>
+                <div className={styles.point_title}>
+                    {`총 포인트: ${totalPoint}점`}
+                </div>
                 {
-                    isLastScript ? (
+                    isLastChat ? (
                         <div className={styles.script_button} onClick={nextClick}>
                             {nextButton}
                         </div>
@@ -90,7 +190,7 @@ export default function Reasoning() {
                     {chats.map((chat, index) => (
                         chat.name === "Conan" ?
                         <Leftchat key={index} name={chat.name} content={chat.content} /> :
-                        <Rightchat key={index} name={chat.name} content={chat.content} /> 
+                        <Rightchat key={index} name={chat.name} content={chat.content} point={chat.point}/> 
                     ))}
                 </div>
 
@@ -100,8 +200,11 @@ export default function Reasoning() {
                     onKeyPress={handleKeyPress}
                     disabled={isDisabled}/>
                     <button style={{ backgroundColor: "inherit", border: "none" }} >
-                        <img src="/images/send.png" alt="send" className={styles.reasoning_image_button}
-                            onClick={sendChat} />
+                        {isLoading ? (
+                            <img src="/images/loading.png" alt="loading" className={styles.reasoning_loading_button} />
+                        ) : (
+                            <img src="/images/send.png" alt="send" className={styles.reasoning_image_button} />
+                        )}
                     </button>
                 </div>
             </div>
