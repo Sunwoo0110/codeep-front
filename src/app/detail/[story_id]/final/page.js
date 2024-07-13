b582e5c07d1dba7dfbf77224c2719e1ec7c30d73
sunwookim <sunkim09@g.skku.edu>
2024-07-13 19:28:34 +0900

    FEAT ranking

--- src/app/detail/[story_id]/final/page.js
+++ src/app/detail/[story_id]/final/page.js
@@ -25,6 +25,10 @@ export default function Final() {
 
     const [storyCluePoint, setStoryCluePoint] = useState(0);
     const [storyDetectPoint, setStoryDetectPoint] = useState(0);
+    const [storyRanking, setStoryRanking] = useState([]);
+    const [totalRanking, setTotalRanking] = useState([])
+    const [storyRank, setStoryRank] = useState(0);
+    const [totalRank, setTotalRank] = useState(0);
 
     const [homeButton, setHomeButton] = useState('처음으로');
     const [storyList, setStoryList] = useState([]);
@@ -57,6 +61,40 @@ export default function Final() {
         window.localStorage.setItem("storyTitle", storyTitle);
         
         window.location.href = `/detail/${storyId}/1`
+    }
+
+    const getRanking = async () => {
+        const story_res = await axios.post(`http://localhost:8000/ranks/story_rank`, {
+            story_id: storyId,
+            name: userName,
+            level: level
+        })
+
+        if (story_res.data.result === "success") {
+            setStoryRanking(story_res.data.rank.rank_list);
+            setStoryRank(story_res.data.rank.target.rank);
+        } else {
+            alert("랭킹 불러오기에 실패하였습니다.")
+        }
+
+        const total_res = await axios.get(`http://localhost:8000/ranks/total_rank/${userName}`)
+        if (total_res.data.result === "success") {
+            setTotalRanking(total_res.data.rank.rank_list);
+            setTotalRank(total_res.data.rank.target.rank);
+        } else {
+            alert("랭킹 불러오기에 실패하였습니다.")
+        }
+    }
+
+    const formatDate = (dateStr) => {
+        const date = new Date(dateStr);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+    
+        return `${year}.${month}.${day} ${hours}:${minutes}`;
     }
 
     useEffect(() => {
@@ -123,7 +161,7 @@ export default function Final() {
         getStats();
         getStoryCluePoint();
         getStoryDetectPoint();
-
+        getRanking();
     }, [])
 
 
@@ -132,35 +170,92 @@ export default function Final() {
             <Header />
             <div className={styles.final_container}>
                 <div className={styles.final_title}>Final Result</div>
-                <div className={styles.final_top}>
-                <div className={styles.final_subtitle}>
-                        스토리 플레이 결과
+                <div className={styles.final_result_container}>
+                    <div className={styles.final_top}>
+                        <div className={styles.final_subtitle}>
+                            스토리 플레이 결과
+                        </div>
+                        <div className={styles.final_storytitle}>
+                            {`${storyTitle} (${(parseInt(level) === 1) ? "초급" : (parseInt(level) === 2) ? "중급" : "고급"})`}
+                        </div>
+                        <div className={styles.final_arresttitle}>
+                            {`${(isArrest === true) ? "범인 검거 성공" : "범인 검거 실패"}`}
+                        </div>
+                        <div className={styles.final_rank}>
+                            {`스토리 순위: ${storyRank}위`}
+                        </div>
+                        <div className={styles.final_text}>
+                            {`단서 포인트: ${storyCluePoint}점\n`}
+                            {`추리 포인트: ${storyDetectPoint}점\n`}
+                            {`플레이 시간: ${totalDetectTime.split(":")[0]}분 ${totalDetectTime.split(":")[1]}초\n`}
+                        </div>
                     </div>
-                    <div className={styles.final_storytitle}>
-                        {`${storyTitle} (${(parseInt(level) === 1) ? "초급" : (parseInt(level) === 2) ? "중급" : "고급"})`}
-                    </div>
-                    <div className={styles.final_arresttitle}>
-                        {`${(isArrest === true) ? "범인 검거 성공" : "범인 검거 실패"}`}
-                    </div>
-                    <div className={styles.final_text}>
-                        {`단서 포인트: ${storyCluePoint}점\n`}
-                        {`추리 포인트: ${storyDetectPoint}점\n`}
-                        {`플레이 시간: ${totalDetectTime.split(":")[0]}분 ${totalDetectTime.split(":")[1]}초\n`}
+
+                    <div className={styles.final_top}>
+                        <div className={styles.final_subtitle}>
+                            총 플레이 결과
+                        </div>
+                        <div className={styles.final_arresttitle}>
+                            {`범인 검거율: ${(completeNum === 0) ? 0 : ((arrestNum / completeNum) * 100).toFixed(2)}%`}
+                        </div>
+                        <div className={styles.final_rank}>
+                            {`전체 순위: ${totalRank}위`}
+                        </div>
+                        <div className={styles.final_text}>
+                            {`전체 단서 포인트: ${totalCluePoint}점\n`}
+                            {`전체 추리 포인트: ${totalDetectPoint}점\n\n`}
+                            {`검거 성공 횟수: ${arrestNum}번\n`}
+                            {`스토리 클리어 횟수: ${completeNum}번\n`}
+                        </div>
                     </div>
                 </div>
 
                 <div className={styles.final_top}>
                     <div className={styles.final_subtitle}>
-                        총 플레이 결과
+                        스토리 플레이 순위
                     </div>
-                    <div className={styles.final_arresttitle}>
-                        {`범인 검거율: ${(completeNum === 0) ? 0 : ((arrestNum / completeNum) * 100).toFixed(2)}%`}
+                    <div className={styles.final_storytitle}>
+                        {`${storyTitle}`}
                     </div>
-                    <div className={styles.final_text}>
-                        {`전체 단서 포인트: ${totalCluePoint}점\n`}
-                        {`전체 추리 포인트: ${totalDetectPoint}점\n\n`}
-                        {`검거 성공 횟수: ${arrestNum}번\n`}
-                        {`스토리 클리어 횟수: ${completeNum}번\n`}
+                    <div className={styles.final_ranking_container}>
+                        {storyRanking.map((rank, index) => {
+                            return (
+                                <div key={index} className={styles.final_ranking_box}>
+                                    <div className={styles.final_ranking_top}>
+                                        <div className={styles.final_ranking_rank}>{`${index+1}위 ${rank.name}`}</div>
+                                        {/* <div className={styles.final_ranking_name}>{rank.name}</div> */}
+                                    </div>
+                                    <div className={styles.final_ranking_bottom}>
+                                        <div className={styles.final_ranking_point}>{rank.story_point}점</div>
+                                        <div className={styles.final_ranking_time}>{formatDate(rank.created_at)}</div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                <div className={styles.final_top}>
+                    <div className={styles.final_subtitle}>
+                        전체 플레이 순위
+                    </div>
+                    <div className={styles.final_ranking_container}>
+                        {
+                            totalRanking.map((rank, index) => {
+                                return (
+                                    <div key={index} className={styles.final_ranking_box}>
+                                        <div className={styles.final_ranking_top}>
+                                            <div className={styles.final_ranking_rank}>{`${index+1}위 ${rank.name}`}</div>
+                                            {/* <div className={styles.final_ranking_name}>{rank.name}</div> */}
+                                        </div>
+                                        <div className={styles.final_ranking_bottom}>
+                                            <div className={styles.final_ranking_point}>{rank.total_story_point}점</div>
+                                            {/* <div className={styles.final_ranking_time}>{formatDate(rank.created_at)}</div> */}
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
 
